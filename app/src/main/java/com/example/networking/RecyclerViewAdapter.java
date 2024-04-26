41f8a762f827cfb485253b553ee8c009bb564c90
Edde2508 <edvin.werner@live.se>
2024-04-26 13:26:42 +0200

    Added code to the RecyclerViewAdapter and textview to show the mountains.

--- app/src/main/java/com/example/networking/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -1,14 +1,60 @@
 package com.example.networking;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-public class RecyclerViewAdapter extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Mountain> items;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+    RecyclerViewAdapter(Context context, List<Mountain> items, OnClickListener onClickListener) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.items = items;
+        this.onClickListener = onClickListener;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler_view_adapter);
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.activity_recycler_view_adapter, parent, false));
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(items.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(items.get(getAdapterPosition()));
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(Mountain item);
     }
 }
